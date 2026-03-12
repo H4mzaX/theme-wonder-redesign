e71d4a4eb8ad562eb5cbe60037ff9b1eeab898b4
H4mzaX <amirhamza7842@gmail.com>
2026-03-13 00:38:35 +0530

    Update shopify.ts

--- src/config/shopify.ts
+++ src/config/shopify.ts
@@ -1,1 +1,8 @@
+export const shopifyConfig = {
+  domain: 'www.vcase.in',
+  storefrontAccessToken: '990cb76e05a8adb2df1d79deadacc65f', // Paste your token from this page
+  apiVersion: '2024-01'
+};
+
+export const SHOPIFY_GRAPHQL_URL = `https://${shopifyConfig.domain}/api/${shopifyConfig.apiVersion}/graphql.json`;
 
